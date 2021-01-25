46353d83fcb777312cfd506dacb6a870268ae97b
fsfalcao <fsfalcao@AGIN110>
2021-01-25 09:42:51 -0300

    Prototipo CRLV-e concluido para analise

--- Dialogs/CRLVeDialog.cs
+++ Dialogs/CRLVeDialog.cs
@@ -26,38 +26,54 @@ namespace Microsoft.BotBuilderSamples.Dialogs
             AddDialog(new DateResolverDialog());
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
             {
+                PlateLicenseStepAsync,
+                SecureCodeStepAsync,
                 InfoStepAsync,
-                YearStepAsync,
-                VencimentoStepAsync,
-
+                FinalStepAsync,
             }));
 
             // The initial child Dialog to run.
             InitialDialogId = nameof(WaterfallDialog);
         }
 
+        private async Task<DialogTurnResult> PlateLicenseStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Para iniciarmos o processo, vou precisar de algumas informações."), cancellationToken);
+            var promptMessage = MessageFactory.Text("Por favor, informe a placa de seu carro", InputHints.ExpectingInput);
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> SecureCodeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["carplate"] = (stepContext.Result.ToString());
+
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Agora, informe o código de segurança"), cancellationToken);
+            string messagetext = null;
+            var secureCode = MessageFactory.Text(messagetext, InputHints.ExpectingInput);
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = secureCode }, cancellationToken);
+
+        }
+
         private async Task<DialogTurnResult> InfoStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Renavam: 49494949\nPlaca: OEK-8859\nProprietário: JOSÉ DA SILVA"), cancellationToken);
+            stepContext.Values["securecode"] = (stepContext.Result.ToString());
+
+            var info = "Placa: " + stepContext.Values["carplate"] + 
+                       "\r\nCódigo de segurança: " + stepContext.Values["securecode"] + 
+                       "\r\nProprietário: JOSÉ DA SILVA";
+
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(info), cancellationToken);
             return await stepContext.ContinueDialogAsync(cancellationToken);
         }
 
-        private async Task<DialogTurnResult> YearStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private static async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            var year = MessageFactory.Text("Qual o ano de exercicio?", InputHints.ExpectingInput);
-            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = year }, cancellationToken);
-        }
-
-        private async Task<DialogTurnResult> VencimentoStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            var promptOptions = new PromptOptions
-            {
-                Prompt = MessageFactory.Text($"Qual dia você quer pagar?"),
-                Choices = ChoiceFactory.ToChoices(new List<string> { "Terça-Feira", "Quarta-Feira", "Quinta-Feira" }),
-            };
-
-            return await stepContext.PromptAsync(nameof(ChoicePrompt), promptOptions, cancellationToken);
-
+            var info = "Aqui está o seu CRLV-e!\r\n" +
+                       "Estou disponibilizando em formato .pdf ou diretamente o código de barras para facilitar seu pagamento!\r\n" +
+                       " - PDF\r\n" +
+                       " - Código de Barras";
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(info), cancellationToken);
+            return await stepContext.EndDialogAsync(cancellationToken);
         }
 
     }

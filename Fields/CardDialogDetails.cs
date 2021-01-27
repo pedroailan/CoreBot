406dcab177c02edd791d93a6abc7d5456f6c923b
Pedro Ailan Silva de OLiveira <pedroaso@dcomp.ufs.br>
2021-01-27 09:54:36 -0300

    Camadas

---
+++ Fields/CardDialogDetails.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class CarDialogDetails
+    {
+        public string Renavam { get; set; }
+
+        public string SecureCode { get; set; }
+        public string MarcaModelo { get; internal set; }
+        public string AnoExercicio { get; internal set; }
+
+        public static explicit operator CarDialogDetails(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Renavam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBot.Models
+{
+    public class Renavam
+    {
+        public static bool ValidationRenavam(string Renavam)
+        {
+            return false;
+        }
+    }
+}

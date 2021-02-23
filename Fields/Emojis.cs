b2953f6c8018049001f040b7e305fa9ab33e67f3
fsfalcao <fsfalcao@AGIN110>
2021-02-23 11:46:30 -0300

    adicao classe emojis

---
+++ Fields/Emojis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBot.Fields
+{
+    public class Emojis
+    {
+        public static string sorriso { get; set; } = "\U0001F600";
+        public static string sorrisoAberto { get; set; } = "\U0001F601";
+        public static string sorrisoLagrima { get; set; } = "\U0001F602";
+        public static string sorrisoAberto2 { get; set; } = "\U0001F603";
+        public static string sorrisoAbertoOlhos { get; set; } = "\U0001F604";
+        public static string sorrisoSuor { get; set; } = "\U0001F605";
+        public static string piscada { get; set; } = "\U0001F609";
+        public static string carro { get; set; } = "\U0001F697";
+
+    }
+}

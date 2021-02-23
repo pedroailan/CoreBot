c5c05d7a750327542a2951373c6f7b6d3c33af90
Pedro Ailan Silva de OLiveira <pedroaso@dcomp.ufs.br>
2021-02-23 08:17:40 -0300

    Classe de emojis

---
+++ Fields/Emojis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBot.Fields
+{
+    public class Emojis
+    {
+        public static string Sorriso { get; set; } = "\U0001F600";
+        public static string Triste { get; set; } = "\U0001F614";
+    }
+}
